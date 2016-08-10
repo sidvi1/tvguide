48a23e7a174f65f8de45f2ae2d1f3ca05e4bf805
sidvi <m00n.post@gmail.com>
2016-08-10 19:21:54 +0300

    Again try to fix travis-ci error in test TestVeTvPlugin while buil

--- tvguide-vsetv-plugin/src/test/java/ru/sidvi/tvguide/plugin/TestVeTvPlugin.java
+++ tvguide-vsetv-plugin/src/test/java/ru/sidvi/tvguide/plugin/TestVeTvPlugin.java
@@ -3,10 +3,7 @@ package ru.sidvi.tvguide.plugin;
 import org.junit.Test;
 import ru.sidvi.tvguide.plugin.parser.VseTvParser;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -22,12 +19,14 @@ public class TestVeTvPlugin {
 
     @Test
     public void testVseTVParser() throws IOException, ParseException {
+        StringWriter writer = new StringWriter();
+        PrintWriter os = new PrintWriter(writer);
+
         InputStream is = getClass().getClassLoader().getResourceAsStream("vsetv_in.html");
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        PrintStream os = new PrintStream(bos);
         List<Event> events = new VseTvParser().parse(is);
+
         printVseTv(events, os);
-        String actual = bos.toString("windows-1251");
+        String actual = writer.getBuffer().toString();
 
         String expected = fromResource("vsetv_out.txt");
 
@@ -38,7 +37,7 @@ public class TestVeTvPlugin {
     }
 
 
-    private static void printVseTv(List<Event> list, PrintStream os) {
+    private static void printVseTv(List<Event> list, PrintWriter os) {
         for (Event event : list) {
             os.println(event.getChannel() + " --- " + format(event.getDate()) + ": " + event.getName());
         }
