bcefe51d6589e1d2f352ff69aecf59234b56a9b7
sidvi <m00n.post@gmail.com>
2016-08-11 15:36:53 +0300

    Refactoring  PluginClassNameDetector to decrease Block Nesting depth

--- tvguide-cmd/src/main/java/ru/sidvi/tvguide/handlers/PluginClassNameDetector.java
+++ tvguide-cmd/src/main/java/ru/sidvi/tvguide/handlers/PluginClassNameDetector.java
@@ -24,30 +24,36 @@ class PluginClassNameDetector {
     }
 
     public boolean detect() {
-
         try {
-            JarFile jarFile = new JarFile(jar);
-            Enumeration allEntries = jarFile.entries();
-            while (allEntries.hasMoreElements()) {
-                JarEntry entry = (JarEntry) allEntries.nextElement();
-                String name = entry.getName();
-
-                if (name.equals("config.properties")) {
-                    InputStream is = jarFile.getInputStream(entry);
-
-                    Properties prop = new Properties();
-                    prop.load(is);
-                    pluginClass = prop.getProperty("plugin.class");
-                    log.debug("Found plugin class: " + pluginClass);
-
-                    return true;
-                }
-            }
+            if (tryToDetect()) return true;
         } catch (IOException e) {
             log.error("Plugin could not be loaded. Sorry.",e);
             return false;
         }
         return false;
+    }
+
+    private boolean tryToDetect() throws IOException {
+        JarFile jarFile = new JarFile(jar);
+        Enumeration allEntries = jarFile.entries();
+        while (allEntries.hasMoreElements()) {
+            JarEntry entry = (JarEntry) allEntries.nextElement();
+            String name = entry.getName();
+
+            if (name.equals("config.properties")) {
+                pluginClass = read(jarFile.getInputStream(entry));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String read(InputStream is) throws IOException {
+        Properties prop = new Properties();
+        prop.load(is);
+        String pluginClass = prop.getProperty("plugin.class");
+        log.debug("Found plugin class: " + pluginClass);
+        return pluginClass;
     }
 
     public String getPlugginClassName() {
